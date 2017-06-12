4d25cdec4b18972a02a31a9e383d01ec4905f2af
Hermann Woock <hermann.woock@oose.de>
2017-06-12 20:27:56 +0200

    initial

---
+++ src/main/java/de/woock/ddd/stattauto/schaden/DddStattAutoSrvSchadensbearbeitungApplication.java
@@ -0,0 +1,12 @@
+package de.woock.ddd.stattauto.schaden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddStattAutoSrvSchadensbearbeitungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddStattAutoSrvSchadensbearbeitungApplication.class, args);
+	}
+}
---
+++ src/test/java/de/woock/ddd/stattauto/schaden/DddStattAutoSrvSchadensbearbeitungApplicationTests.java
@@ -0,0 +1,16 @@
+package de.woock.ddd.stattauto.schaden;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DddStattAutoSrvSchadensbearbeitungApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
